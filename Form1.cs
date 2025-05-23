2b49dc2fc5070b145ff9ddb2285d8e07c91aa560
LlewellynJonkers <l.jonkers220@gmail.com>
2025-05-23 12:43:11 +0200

    Click control bit complete

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace Mouse_Clicker
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RadCheck_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
